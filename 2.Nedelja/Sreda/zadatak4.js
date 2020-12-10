e9088380f64a3837b310d1bb3cfc9d528bd65b97
petarLecic <petar.lecic94@gmail.com>
2020-12-10 13:45:43 +0100

    Ispravka 4. zadatka

--- 2.Nedelja/Sreda/zadatak4.js
+++ 2.Nedelja/Sreda/zadatak4.js
@@ -1,5 +1,7 @@
-for (let i = 1; i <= 1000; i++) {
+let counter = 0;
+for (let i = 1; counter <= 1000; i++) {
     if (i % 5 == 0) {
+        counter++
         console.log(i);
     }
-}
+}                      
