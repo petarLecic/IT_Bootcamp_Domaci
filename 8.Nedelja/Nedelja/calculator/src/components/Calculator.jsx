193e87c8eb291b52e300dfa4f6eb54dd61af863c
petarLecic <petar.lecic94@gmail.com>
2021-02-18 17:14:15 +0100

    refaktorisane komponente

--- 8.Nedelja/Nedelja/calculator/src/components/Calculator.jsx
+++ 8.Nedelja/Nedelja/calculator/src/components/Calculator.jsx
@@ -6,7 +6,31 @@ const Calculator = () => {
     const [inputNumber, setInputNumber] = useState('')
     const [steps, setSteps] = useState(result)
     const [lastOp, setLastOp] = useState('')
-    
+
+    const stateSetter = operation => {
+        //Setting result
+        if (inputNumber === 0) setResult(0) // Izbegava pojavu "Infinity" i "NaN" u deljenju sa 0
+        else switch (operation) {
+            case '+': setResult(prev => Number(prev) + Number(inputNumber)); break;
+            case '-': setResult(prev => Number(prev) - inputNumber); break;
+            case '*': setResult(prev => Number(prev) * inputNumber); break;
+            case '/': setResult(prev => Number(prev) / inputNumber); break;
+            default: setResult(0); break;
+        }
+        //Setting steps
+        operation === 'Clear' ? setSteps(0) : setSteps(prev => {
+                    if ((lastOp === '+' || lastOp === '-') && (operation === '*' || operation === '/')) {
+                        return `(${prev}) ${operation} ${inputNumber}`
+                    }
+                    else {
+                        return `${prev} ${operation} ${inputNumber}`
+                    }
+                })
+        
+        setInputNumber('')
+        setLastOp(operation)
+    }
+
     return (
         <>
             <h1>Calculator</h1>
@@ -14,46 +38,11 @@ const Calculator = () => {
                 <input value={result} readOnly/>
                 <input type="number" value={inputNumber} onChange={e => setInputNumber(e.target.value)} />
             </div>
-            <OperationBtn 
-                operation="+" 
-                input={inputNumber} 
-                setInput={setInputNumber} 
-                setResult={setResult} 
-                setSteps={setSteps} 
-                lastOp={lastOp} 
-                setLastOp={setLastOp}
-                />
-            <OperationBtn operation="-" 
-                input={inputNumber} 
-                setInput={setInputNumber} 
-                setResult={setResult} 
-                setSteps={setSteps} 
-                lastOp={lastOp} 
-                setLastOp={setLastOp}
-                />
-            <OperationBtn operation="*" 
-                input={inputNumber} 
-                setInput={setInputNumber} 
-                setResult={setResult} 
-                setSteps={setSteps} 
-                lastOp={lastOp} 
-                setLastOp={setLastOp}
-                />
-            <OperationBtn operation="/" 
-                input={inputNumber} 
-                setInput={setInputNumber} 
-                setResult={setResult} 
-                setSteps={setSteps} 
-                lastOp={lastOp} 
-                setLastOp={setLastOp}
-                />
-            <OperationBtn 
-                operation="Clear" 
-                setInput={setInputNumber} 
-                setResult={setResult} 
-                setSteps={setSteps} 
-                setLastOp={setLastOp}
-            />
+            <OperationBtn operation="+" stateSetter={stateSetter} />
+            <OperationBtn operation="-" stateSetter={stateSetter} />
+            <OperationBtn operation="*" stateSetter={stateSetter} />
+            <OperationBtn operation="/" stateSetter={stateSetter} />
+            <OperationBtn operation="Clear" stateSetter={stateSetter} />
             <p>{steps}</p>
         </>
     )
--- 8.Nedelja/Nedelja/calculator/src/components/OperationBtn.jsx
+++ 8.Nedelja/Nedelja/calculator/src/components/OperationBtn.jsx
@@ -1,32 +1,7 @@
-const OperationBtn = ({ operation, input, setInput, setResult, setSteps, lastOp, setLastOp }) => {
-    input = Number(input)
-    
+const OperationBtn = ({ operation, stateSetter }) => {
     return (
-        <button 
-            onClick={() => {
-                if (input === 0) setResult(0) // Izbegava pojavu "Infinity" i "NaN" u deljenju sa 0
-                else switch (operation) {
-                    case '+': setResult(prev => Number(prev) + input); break;
-                    case '-': setResult(prev => Number(prev) - input); break;
-                    case '*': setResult(prev => Number(prev) * input); break;
-                    case '/': setResult(prev => Number(prev) / input); break;
-                    default: setResult(0); break;
-                }
-
-                if (operation === 'Clear') setSteps(0)
-                else setSteps(prev => {
-                    if ((lastOp === '+' || lastOp === '-') && (operation === '*' || operation === '/')) {
-                        return `(${prev}) ${operation} ${input}`
-                    }
-                    else {
-                        return `${prev} ${operation} ${input}`
-                    }
-                })
-                
-                setInput('')
-                setLastOp(operation)
-            }}
-            >{operation}
+        <button onClick={() => stateSetter(operation)}>
+            {operation}
         </button>
     )
 }
